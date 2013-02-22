796e2e6f77375cf9a5e6b27ced822b5143d3821b
abkahrs <abkahrs@gmail.com>
2013-02-22 11:21:11 -0600

    Update launcher.java

--- launcher.java
+++ launcher.java
@@ -1,4 +1,28 @@
-public static void main(String[] args)
-runtime get.runtime.exec(/c "start iexplore.exe");
-if{
-	
+public static void main(String[] args){
+try{
+	runtime get.runtime.exec("iexplore.exe");
+ } catch (FileNotFoundException e) {
+    System.err.println("FileNotFoundException: " + e.getMessage());
+    throw new SampleException(e);
+
+} catch (IOException e) {
+    System.err.println("Caught IOException: " + e.getMessage());
+}else{
+	try{
+		runtime get.runtime.exec("iexplore.exe");
+}catch (FileNotFoundException e) {
+    System.err.println("FileNotFoundException: " + e.getMessage());
+    throw new SampleException(e);
+
+} catch (IOException e) {
+    System.err.println("Caught IOException: " + e.getMessage());} 
+}else{
+	try{
+		runtime get.runtime.exec("iexplore.exe");
+}catch (FileNotFoundException e) {
+    System.err.println("FileNotFoundException: " + e.getMessage());
+    throw new SampleException(e);
+
+} catch (IOException e) {
+    System.err.println("Caught IOException: " + e.getMessage());} }	
+	}
